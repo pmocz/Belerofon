d52bfb20a40bf57c2e53b6c0f1d1231d0240e04c
pmocz <philip.mocz@gmail.com>
2018-09-05 16:24:53 -0400

    tiger sbatch update

--- sbatch_tigress.sh
+++ sbatch_tigress.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-#SBATCH -J B64
+#SBATCH -J bfn512
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=40
-#SBATCH -t 0:00:10
+#SBATCH -t 0:12:00
 #SBATCH -o output/OUTPUT.lsf
 #SBATCH -e output/ERROR.lsf
 #SBATCH --mail-user=philip.mocz@gmail.com
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --mem=8000     # 80000 for 1024^3 run
+#SBATCH --mem=10000     # 80000 for 1024^3 run
 
 module purge
 module load intel
 module load openmpi
 module load fftw
 
-./belerofon 40
+srun ./belerofon 40 > output/OUTPUT.$SLURM_JOB_ID
 
 
