e78addd2db6cc31fd0ca13de704eade234e2d836
pmocz <philip.mocz@gmail.com>
2018-09-05 15:56:03 -0400

    sbatch update

--- sbatch_tigress.sh
+++ sbatch_tigress.sh
@@ -2,14 +2,14 @@
 #SBATCH -J B64
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=40
-#SBATCH -t 0:02:00
+#SBATCH -t 0:00:10
 #SBATCH -o output/OUTPUT.lsf
 #SBATCH -e output/ERROR.lsf
 #SBATCH --mail-user=philip.mocz@gmail.com
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --mem=80000
+#SBATCH --mem=8000     # 80000 for 1024^3 run
 
 module purge
 module load intel
